d40e02db9f17c9490991740dddef0a2acfd531f2
frmmdd <96875393+frmmdd@users.noreply.github.com>
2023-07-10 20:24:35 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+  import Card from "./components/Card.vue"
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <Card v-for="card in cards"
+        :style="{ 
+          background: card.color,
+          color: card.color
+        }"
+        :icon="card.icon"
+        :title="card.title"
+        :content="card.content"
+      />
+    </div>
+  </main>
+</template>
+
+<script>
+import { cards } from "./data.js"
+  
+  export default {
+    data () {
+      return {
+        cards: cards
+      }
+    },
+  }
+  
+</script>
+
+
+<style scoped>
+  
+  .container {
+    display: grid;
+    grid-template-rows: auto;
+  }
+
+  @media screen and (min-width: 490px) {
+    .container {
+      grid-template-columns: repeat(2, 240px);
+    }
+  }
+
+  @media screen and (min-width: 750px) {
+    .container {
+      grid-template-columns: repeat(3, 240px);
+    }
+  }
+  
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,56 @@
+<script setup>
+  
+  defineProps({
+    icon: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    }
+  })
+  
+</script>
+
+<template>
+  <div class="card">
+    <img :src="icon" alt="icon-car" />
+    <div class="wraper">
+      <h1>{{title}}</h1>
+      <p>{{content}}</p>
+      <a class="button" href="#">Learn More</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+  .card {
+    max-width: 240px;
+    height: -webkit-fill-available;
+    padding: 3.5em 2em;
+  }
+
+  .wraper {
+    height: -webkit-fill-available;
+    display: flex;
+    flex-direction: column;
+    gap: 5em;
+    padding: 2em 0 1em 0;
+  }
+
+  .button {
+    width: fit-content;
+    color: inherit !important;
+    background: hsl(0, 0%, 95%);
+    padding: 0.7em 1em;
+    border-radius: 100px;
+    text-decoration: none;
+  }
+  
+</style>
